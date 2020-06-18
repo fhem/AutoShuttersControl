cb7c3f8667b7e48dfaccca28021c08fe556d79f8
Marko Oldenburg <marko.oldenburg@cooltux.net>
2020-06-18 08:13:48 +0200

    new mail

--- lib/FHEM/Automation/ShuttersControl.pm
+++ lib/FHEM/Automation/ShuttersControl.pm
@@ -2,7 +2,7 @@
 #
 # Developed with Kate
 #
-#  (c) 2018-2020 Copyright: Marko Oldenburg (leongaultier at gmail dot com)
+#  (c) 2018-2020 Copyright: Marko Oldenburg (fhemsupport@cooltux.net)
 #  All rights reserved
 #
 #   Special thanks goes to:
