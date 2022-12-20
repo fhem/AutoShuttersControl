9cd0d364b4e679b2baef25c647a5bcd12e81455d
Marko Oldenburg <fhemdevelopment@cooltux.net>
2022-12-20 14:17:08 +0100

    change Copyright text
    new year range

--- FHEM/73_AutoShuttersControl.pm
+++ FHEM/73_AutoShuttersControl.pm
@@ -2,7 +2,7 @@
 #
 # Developed with VSCodium and richterger perl plugin
 #
-#  (c) 2018-2022 Copyright: Marko Oldenburg (fhemdevelopment at cooltux dot net)
+#  (c) 2018-2023 Copyright: Marko Oldenburg (fhemdevelopment at cooltux dot net)
 #  All rights reserved
 #
 #   Special thanks goes to:
@@ -1462,7 +1462,7 @@ __END__
   ],
   "release_status": "stable",
   "license": "GPL_2",
-  "version": "v0.10.24",
+  "version": "v0.10.25",
   "author": [
     "Marko Oldenburg <fhemdevelopment@cooltux.net>"
   ],
